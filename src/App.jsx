316426f78072b9cd9b8166c9535113cdf695a113
MikeWaser <mik3waser@gmail.com>
2024-02-28 13:36:06 +0100

    steg 4

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+
+const App = () => {
+  return (
+    <>
+    <p>Paragraph text</p>
+     < Header headerText = "Steg 4 klar"/>
+    </>
+  )
+}
+
+export default App
